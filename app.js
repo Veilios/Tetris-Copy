67aff8f1cd74b0d125674d3dc487a62c7111f31a
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-10-28 20:51:29 -0400

    Made the initial variablesin js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const grid = document.querySelector('.gird');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const width = 10;
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button');
 
 
-    
+
+
 });
