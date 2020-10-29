b9c49e90cefdac080b9e7738974a7cc842b09671
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-10-28 21:17:58 -0400

    Created a function to undraw the tetromino

--- app.js
+++ app.js
@@ -12,50 +12,61 @@ document.addEventListener('DOMContentLoaded', () => {
         [width, width+1, width+2, width*2+2],
         [1, width+1, width*2+1, width*2],
         [width, width*2, width*2+1, width*2+2]
-    ];
+    ]
     
-    const zTetromino = [
+      const zTetromino = [
         [0,width,width+1,width*2+1],
         [width+1, width+2,width*2,width*2+1],
         [0,width,width+1,width*2+1],
         [width+1, width+2,width*2,width*2+1]
-    ];
-
-    const tTetromino = [
+    ]
+    
+      const tTetromino = [
         [1,width,width+1,width+2],
         [1,width+1,width+2,width*2+1],
         [width,width+1,width+2,width*2+1],
         [1,width,width+1,width*2+1]
-    ];
-
-    const oTetromino = [
+    ]
+    
+      const oTetromino = [
         [0,1,width,width+1],
         [0,1,width,width+1],
         [0,1,width,width+1],
         [0,1,width,width+1]
-    ];
-
-    const iTetromino = [
+    ]
+    
+      const iTetromino = [
         [1,width+1,width*2+1,width*3+1],
         [width,width+1,width+2,width+3],
         [1,width+1,width*2+1,width*3+1],
         [width,width+1,width+2,width+3]
-    ];
+    ]
+    
 
     // Array of Tetrominoes
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
     let currentPosition = 4;
-    let current = theTetrominoes[0][0];
+    let currentRotation = 0;
 
-    // Draw the first rotation in the first tetromino
+    // Randomly select a Tetromino and its first rotation
+    let random = Math.floor(Math.random()*theTetrominoes.length);
+    let current = theTetrominoes[random][currentRotation];
+
+    // Draw a random tetromino
     const draw = () => {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino');
         });
     };
 
-    draw();
+    
+   // Undraw the Tetromino
+   const undraw = () => {
+       current.forEach(index => {
+           squares[currentPosition + index].classList.remove('tetromino');
+       })
+   }; 
 
 
 
