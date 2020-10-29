94567405f58f5d2d80454cfa5c4010b9b9797466
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-10-28 21:10:36 -0400

    Created a function to be able to draw the first tetromino on the grid

--- app.js
+++ app.js
@@ -19,30 +19,43 @@ document.addEventListener('DOMContentLoaded', () => {
         [width+1, width+2,width*2,width*2+1],
         [0,width,width+1,width*2+1],
         [width+1, width+2,width*2,width*2+1]
-    ]
+    ];
 
     const tTetromino = [
         [1,width,width+1,width+2],
         [1,width+1,width+2,width*2+1],
         [width,width+1,width+2,width*2+1],
         [1,width,width+1,width*2+1]
-    ]
+    ];
 
     const oTetromino = [
         [0,1,width,width+1],
         [0,1,width,width+1],
         [0,1,width,width+1],
         [0,1,width,width+1]
-    ]
+    ];
 
     const iTetromino = [
         [1,width+1,width*2+1,width*3+1],
         [width,width+1,width+2,width+3],
         [1,width+1,width*2+1,width*3+1],
         [width,width+1,width+2,width+3]
-    ]
+    ];
 
+    // Array of Tetrominoes
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
+    let currentPosition = 4;
+    let current = theTetrominoes[0][0];
+
+    // Draw the first rotation in the first tetromino
+    const draw = () => {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino');
+        });
+    };
+
+    draw();
 
 
 
