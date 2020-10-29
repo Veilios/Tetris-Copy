88a9125b9e1e948fa2348c2812b963d0c3a421b2
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-10-29 19:54:03 -0400

    Created function for tetromino to move right

--- app.js
+++ app.js
@@ -82,9 +82,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Assign functions to keyCodes
     const control = (e) => {
-        if(e.keyCode === 37){
+        if(e.keyCode === 37) {
             moveLeft();
-        };
+        } else if  (e.keyCode === 38) {
+            //rotate()
+        } else if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 40) {
+            //moveDown()
+        }
     };
     document.addEventListener('keyup', control);
 
@@ -111,6 +117,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
             currentPosition += 1;
+        };
+
+        draw();
+    };
+
+
+    // Move the tetromino right, unless is at the edge or there is a blockage
+    const moveRight = () => {
+        undraw();
+        const isAtRightEdge = current.some(index => (currentPosition +index) % width === width - 1);
+
+        if(!isAtRightEdge) currentPosition +=1;
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1;
         };
 
         draw();
