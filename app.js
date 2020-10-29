10d8912625bf647fcda0a1cd9dd1627e0ce09078
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-10-29 19:58:55 -0400

    Created function for the tetromino to rotate if you press the up key

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if(e.keyCode === 37) {
             moveLeft();
         } else if  (e.keyCode === 38) {
-            //rotate()
+            rotate()
         } else if (e.keyCode === 39) {
             moveRight()
         } else if (e.keyCode === 40) {
@@ -137,6 +137,16 @@ document.addEventListener('DOMContentLoaded', () => {
         draw();
     };
 
+    // Rotate the tetromino
+    const rotate = () => {
+        undraw();
+        currentRotation ++;
+        if(currentRotation === current.length) { // If the current rotation gets to 4, make it go back to 0
+            currentRotation = 0;
+        };
+        current = theTetrominoes[random][currentRotation];
+        draw();
+    };
 
 
 
